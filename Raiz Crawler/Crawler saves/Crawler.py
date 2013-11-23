2e8bb76e9bf8fdcc2f756a7ec85f7fe920d0f290
Carlos Roberto das Chagas junior <carlos_chagas@hotmail.com>
2013-11-23 13:49:21 -0200

    Padronização de pastas + documentação

---
+++ Raiz Crawler/Crawler saves/Crawler.py
@@ -0,0 +1,86 @@
+# -*- coding: cp1252 -*-
+from bs4 import BeautifulSoup
+import urllib
+import urllib2
+import urlparse
+import Tempo_Carregamento
+import Status_Code
+
+def main(siteurl):
+    #Site do crawler
+    #siteurl = raw_input('Digite o site desejado(Com http://): ')
+    link = siteurl.strip()
+    final = link
+
+    if (final.count('/')>2):
+        if (final[:7] == 'http://'):
+            aux = final[7:]
+            pos = aux.find('/') + 8
+            main =final[:pos]
+
+        elif (final[:8] == 'https://'):
+            aux = final[8:]
+            pos = aux.find('/') + 9
+            main =final[:pos]
+    elif (final.count('/')==2):
+        main = final
+
+    htmltext = urllib.urlopen(final)
+    soup = BeautifulSoup(htmltext)
+    listaURL = [final]
+
+    for tag in soup.findAll('a',href=True):
+        raw = tag['href']
+        b1 = urlparse.urlparse(tag['href']).hostname
+        if b1 == None:        
+            b1 = main
+        b2 = urlparse.urlparse(tag['href']).path
+        newurl = str(b1) + str(b2)
+        #cria uma lista com todas urls
+        listaURL.append(newurl)
+    
+    #Mostra a lista de URL's
+    for list in listaURL:
+        print(list)
+    return listaURL
+
+#função para calcular a latencia	
+def latencia(final):
+
+    try:
+        print("Chamando função para calcular a latencia")
+        latencia = Tempo_Carregamento.PingTest(final)
+        print(str(latencia)+" ms")
+    except ValueError:
+        try:
+            a = main[:len(main)-1]
+            latencia = Tempo_Carregamento.PingTest(a)
+            print(str(latencia)+" ms")
+        except ValueError:
+            print "Não foi possivel calcular o tempo de latencia"
+            latencia = None
+    return latencia
+            
+#função para calcular o tempo de load da pagina
+def loadTime(final):
+    print("Chamando função para calcular o tempo de load da pagina")
+    loadTime = Tempo_Carregamento.WebLoadTime(final)
+    print(str(loadTime)+" sec")
+    return loadTime
+
+#Status_Code
+def StatusCode(listaURL):
+    print ("Chamando Status Code")
+    codeList = Status_Code.VerificaStatus(listaURL)
+    for list in codeList:
+        print list
+    return codeList
+
+#Verifica que tipo de código que é
+def StatusCodeMSG(codeList):
+    print ("Verificando o código")
+    codeListMsg = Status_Code.StatusCodeList(codeList)
+    for list in codeListMsg:
+        print list
+    return codeListMsg
+
---
+++ Raiz Crawler/Crawler saves/a.py
@@ -0,0 +1,18 @@
+import urllib
+import urllib2
+import urlparse
+import Crawler
+
+siteurl = raw_input('Digite o site desejado(Com http://): ')
+link = siteurl.strip()
+final = link
+#Retorna a lista de URLs geradas
+listaURL = Crawler.main(siteurl)
+#Retorna o ping da URL 'principal'
+latencia = Crawler.latencia(final)
+#Retorna o tempo de carregamento
+loadTime = Crawler.loadTime(final)
+#Retorna os codigos das URLs
+StatusCode = Crawler.StatusCode(listaURL)
+#Retorna a Descrição dos codigos
+StatusCodeMsg = Crawler.StatusCodeMSG(StatusCode)
